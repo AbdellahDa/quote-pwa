3c7041f5c19a24948f06d41936150840d055fcdd
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-06-14 23:06:09 +0200

    service worker aangepast workbox toegevoegd en verplaatst naar src folder

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,10 @@
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.routing.registerRoute(
+    ({url}) => url.origin === 'https://api.quotable.io',
+    new workbox.strategies.NetworkFirst()
+);
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
