df1bb36a3062cc3caf9682cc0a7bb70cb17a332b
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-06-15 14:15:34 +0200

    serviceWorkerRegistration.js aangepast

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,9 +1,6 @@
-// Check if service workers are supported in the browser
 if ('serviceWorker' in navigator) {
-    // Wait until the page is fully loaded
     window.addEventListener('load', async () => {
         try {
-            // Register the service worker
             const registration = await navigator.serviceWorker.register('/service-worker.js');
             console.log('Service Worker registered:', registration);
         } catch (error) {
