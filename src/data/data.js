34c40622a7b731f4fff6cd6253efb4b7769e8fbb
Abdellah Dahmani <r0880228@student.thomasmore.be>
2023-06-15 13:56:26 +0200

    data bestand aan gemaakt voor de quotes

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+const quotes = [
+    {
+        id: 1,
+        content: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+    {
+        id: 2,
+        content: "In the middle of difficulty lies opportunity.",
+        author: "Albert Einstein"
+    },
+    {
+        id: 3,
+        content: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt"
+    },
+    {
+        id: 4,
+        content: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        id: 5,
+        content: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston Churchill"
+    },
+];
+
+export default quotes;
